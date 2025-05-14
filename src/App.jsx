ffdfb34d114e57657282a0e04cb1a917abb0634c
Javi <Jjaviergonzalez23@gmail.com>
2025-05-14 17:06:18 +0200

    'moreClean'

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 import { getFilms } from './utils/getFilms'
 import { genres, genresNamesFind } from './utils/genres'
 import { GenreComponent } from './components/GenreComponent'
+import { getColorByVote } from './utils/getColorByVote'
 
 function App() {
 
   const [film, setFilm] = useState(null)
-  const [genresId, setGenresId] = useState([])
   const [genresNames, setGenresNames] = useState([])
 
   const baseUrl = "https://image.tmdb.org/t/p/w500"
@@ -17,7 +17,6 @@ function App() {
 
       const filmData = await getFilms()
       setFilm(filmData)
-      setGenresId(filmData.genre_ids)
       const newNamesGenres = genresNamesFind(filmData.genre_ids);
       setGenresNames(newNamesGenres)
 
@@ -25,20 +24,6 @@ function App() {
       console.error(err)
     }
   }
-
-  function getVoteByColor(vote){
-    if(vote >= 8){
-      return 'green'
-
-    }else if(vote >= 5){
-      return 'orange'
-
-    }else{
-      return 'red'
-    }
-  }
-
-  console.log(film)
 
   return (
     <div className='container'>
@@ -52,7 +37,7 @@ function App() {
             <div className='film'>
               <div className='title-container'>
                 <h2 className='title'>{film.title}</h2>
-                <h2 className='vote' style={{color: getVoteByColor(film.vote_average)}}>{film.vote_average.toFixed(1)}</h2>
+                <h2 className='vote' style={{color: getColorByVote(film.vote_average)}}>{film.vote_average.toFixed(1)}</h2>
               </div>
               {film.release_date.slice(0, 4)}
               <div className='genre-container'>
---
+++ src/utils/getColorByVote.js
@@ -0,0 +1,11 @@
+export function getColorByVote(vote){
+    if(vote >= 8){
+      return 'green'
+
+    }else if(vote >= 5){
+      return 'orange'
+
+    }else{
+      return 'red'
+    }
+  }
